9d69ca1413dc689052888cf7be406cfe8474386f
Andrew Watts <awatts2@ur.rochester.edu>
2017-03-20 10:55:00 -0400

    Unify license
      * Realized that files had mix of BSD, MIT, and LGPL licenses. Changed all the ones I wrote to MIT for consistencies sake and to match LICENSE file.
      * Also added __author__ to all the files

--- blockWorkers.py
+++ blockWorkers.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 
-#Author: Andrew Watts
+# Copyright (c) 2012-2017 Andrew Watts and the University of Rochester BCS Department
 #
-#    Copyright 2012 Andrew Watts and
-#        the University of Rochester BCS Department
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License version 2.1 as
-#    published by the Free Software Foundation.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program.
-#    If not, see <http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html>.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
 
 from __future__ import print_function
@@ -24,6 +26,8 @@ from boto.mturk.connection import MTurkConnection
 from csv import DictReader
 import argparse
 from os.path import expanduser
+
+__author__ = 'Andrew Watts <awatts2@ur.rochester.edu>'
 
 parser = argparse.ArgumentParser(description='Block a worker from doing your HITs on Amazon Mechanical Turk')
 parser.add_argument('-blockfile', required=True, help="(required) File with comma separated 'worker' and 'reason' columns")
--- getHitDetails.py
+++ getHitDetails.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 
-#Author: Andrew Watts
+# Copyright (c) 2012-2017 Andrew Watts and the University of Rochester BCS Department
 #
-#    Copyright 2012 Andrew Watts and
-#        the University of Rochester BCS Department
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License version 2.1 as
-#    published by the Free Software Foundation.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program.
-#    If not, see <http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html>.
-#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 """
 Get information about a HIT, including its completion status.
@@ -31,6 +32,8 @@ from boto.mturk.connection import MTurkConnection
 from csv import DictReader
 import argparse
 from os.path import expanduser
+
+__author__ = 'Andrew Watts <awatts2@ur.rochester.edu>'
 
 ########################################################################
 # A couple of functions borrowed from boto's "mturk" command line app
